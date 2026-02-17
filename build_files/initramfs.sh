4860169589431fb9f2069d8b6ab3a27a3224e31e
Microwave <121560464+Micro856@users.noreply.github.com>
2026-02-17 21:28:20 +0100

    Update and rename build.sh to initramfs.sh

---
+++ build_files/initramfs.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/bash
+# Borrowed from https://github.com/ublue-os/aurora/blob/main/build_files/base/19-initramfs.sh
+
+echo "::group:: ===$(basename "$0")==="
+
+set -oue pipefail
+
+KERNEL_VERSION=$(rpm -q --queryformat="%{evr}.%{arch}" kernel-core)
+
+# Ensure Initramfs is generated
+export DRACUT_NO_XATTR=1
+/usr/bin/dracut --no-hostonly --kver "${KERNEL_VERSION}" --reproducible -v --add "ostree fido2 tpm2-tss pkcs11 pcsc" -f "/lib/modules/${KERNEL_VERSION}/initramfs.img"
+chmod 0600 "/lib/modules/${KERNEL_VERSION}/initramfs.img"
+
+echo "::endgroup::"
