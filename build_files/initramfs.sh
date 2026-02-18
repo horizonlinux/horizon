5baf0c99209783de1327e03126d96cad01440b53
Microwave <121560464+Micro856@users.noreply.github.com>
2026-02-18 07:38:09 +0100

    Update initramfs.sh

--- build_files/initramfs.sh
+++ build_files/initramfs.sh
@@ -9,7 +9,7 @@ KERNEL_VERSION=$(rpm -q --queryformat="%{evr}.%{arch}" kernel-core)
 
 # Ensure Initramfs is generated
 export DRACUT_NO_XATTR=1
-/usr/bin/dracut --regenerate-all --no-hostonly --kver "${KERNEL_VERSION}" --reproducible -v --add "ostree plymouth" -f "/lib/modules/${KERNEL_VERSION}/initramfs.img"
+/usr/bin/dracut --no-hostonly --kver "${KERNEL_VERSION}" --reproducible -v --add "ostree plymouth" -f "/lib/modules/${KERNEL_VERSION}/initramfs.img"
 chmod 0600 "/lib/modules/${KERNEL_VERSION}/initramfs.img"
 
 echo "::endgroup::"
