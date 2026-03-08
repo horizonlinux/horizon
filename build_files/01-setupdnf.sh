40dcc33a0597e72ddbf42c8b49f9d251fd898b5c
Marcel Mrówka <micro.mail88@gmail.com>
2026-03-08 19:29:52 +0100

    Fix "KDE" not avaible after fixing batteries

--- build_files/01-setupdnf.sh
+++ build_files/01-setupdnf.sh
@@ -5,10 +5,10 @@ set -eoux pipefail
 echo "::group:: Setup DNF"
 
 dnf config-manager --set-enabled crb && dnf install -y https://dl.fedoraproject.org/pub/epel/epel-release-latest-10.noarch.rpm
+dnf install -y --nogpgcheck https://mirrors.rpmfusion.org/free/el/rpmfusion-free-release-10.noarch.rpm
 dnf update -y
 dnf -y copr enable horizonproject/horizon
 dnf -y copr enable ublue-os/packages
-dnf install -y --nogpgcheck https://mirrors.rpmfusion.org/free/el/rpmfusion-free-release-10.noarch.rpm
 dnf update -y
 
 echo "::endgroup::"
--- build_files/03-KDE.sh
+++ build_files/03-KDE.sh
@@ -4,6 +4,8 @@ set -eoux pipefail
 
 echo "::group:: Install KDE Plasma"
 
+dnf install -y https://dl.fedoraproject.org/pub/epel/epel-release-latest-10.noarch.rpm
+dnf update -y
 dnf group install -y KDE -x *sddm* -x sddm -x sddm-kcm -x sddm-breeze -x kdebugsettings -x krfb -x plasma-discover -x plasma-discover-notifier -x kde-settings-sddm -x kde-settings-pulseaudio -x kde-settings -x kde-settings-plasma
 
 dnf install -y \
