eb7349cddc3c8c21624783d8dedd39b87ebba155
Microwave <121560464+Micro856@users.noreply.github.com>
2026-03-08 13:53:25 +0100

    Update 03-KDE.sh

--- build_files/03-KDE.sh
+++ build_files/03-KDE.sh
@@ -7,6 +7,7 @@ echo "::group:: Install KDE Plasma"
 dnf group install -y KDE -x *sddm* -x sddm -x sddm-kcm -x sddm-breeze -x kdebugsettings -x krfb -x plasma-discover -x plasma-discover-notifier -x kde-settings-sddm -x kde-settings-pulseaudio -x kde-settings -x kde-settings-plasma
 
 dnf install -y \
+        drkonqi \
         dolphin \
         konsole \
         horizon-plasma-setup \
