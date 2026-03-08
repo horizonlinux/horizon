af0ebda5a2ed9e2c4726faa93ca2639fcb4b0ca1
Microwave <121560464+Micro856@users.noreply.github.com>
2026-03-08 11:16:06 +0100

    Update 03-KDE.sh

--- build_files/03-KDE.sh
+++ build_files/03-KDE.sh
@@ -4,7 +4,7 @@ set -eoux pipefail
 
 echo "::group:: Install KDE Plasma"
 
-dnf group install -y KDE -x sddm -x sddm-kcm -x kdebugsettings -x krfb -x plasma-discover -x plasma-discover-notifier -x kde-settings-sddm -x kde-settings-pulseaudio -x kde-settings -x kde-settings-plasma
+dnf group install -y KDE -x *sddm* -x sddm -x sddm-kcm -x sddm-breeze -x kdebugsettings -x krfb -x plasma-discover -x plasma-discover-notifier -x kde-settings-sddm -x kde-settings-pulseaudio -x kde-settings -x kde-settings-plasma
 
 dnf install -y \
         dolphin \
