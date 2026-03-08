e783b17f3cc309c0cb1d9b68f929bb407eaebbaa
Marcel Mrówka <micro.mail88@gmail.com>
2026-03-08 19:16:39 +0100

    Fix "Batteries"

--- build_files/05-fixdirs.sh
+++ build_files/05-fixdirs.sh
@@ -9,6 +9,8 @@ echo "d $(find /var/lib/dnf/repos -maxdepth 1 -name 'baseos-*') 0755 root root -
 echo "d $(find /var/lib/dnf/repos -maxdepth 1 -name 'crb-*') 0755 root root - -" | tee -a /usr/lib/tmpfiles.d/dnf.conf
 echo "d $(find /var/lib/dnf/repos -maxdepth 1 -name 'epel-*') 0755 root root - -" | tee -a /usr/lib/tmpfiles.d/dnf.conf
 echo "d $(find /var/lib/dnf/repos -maxdepth 1 -name 'extras-common-*') 0755 root root - -" | tee -a /usr/lib/tmpfiles.d/dnf.conf
+echo "d $(find /var/lib/dnf/repos -maxdepth 1 -name 'rpmfusion*') 0755 root root - -" | tee -a /usr/lib/tmpfiles.d/dnf.conf
+echo "d $(find /var/lib/dnf/repos -maxdepth 1 -name 'terra*') 0755 root root - -" | tee -a /usr/lib/tmpfiles.d/dnf.conf
 echo "d /var/lib/flatpak 0755 root root - -" | tee -a /usr/lib/tmpfiles.d/flatpak.conf
 echo "d /var/lib/openvpn 0770 openvpn openvpn - -" | tee -a /usr/lib/tmpfiles.d/openvpn.conf
 echo "d /var/lib/samba/drivers 0775 root printadmin - -" | tee -a /usr/lib/tmpfiles.d/samba.conf
