fa918cf8bb2ef81f53d2499d1802f76c48a36560
Microwave <121560464+Micro856@users.noreply.github.com>
2026-03-09 16:01:14 +0100

    Update 02-basesystem.sh

--- build_files/02-basesystem.sh
+++ build_files/02-basesystem.sh
@@ -4,7 +4,7 @@ set -eoux pipefail
 
 echo "::group:: Base System Packages"
 
-dnf install -y horizon-logos horizon-backgrounds horizon-themes google-noto-color-emoji-fonts
+dnf install -y horizon-logos horizon-backgrounds horizon-themes google-noto-color-emoji-fonts opendyslexic-fonts
 dnf install -y https://repos.fyralabs.com/terrael10/nerdfontssymbolsonly-nerd-fonts-0%3A3.4.0-1.el10.noarch.rpm
 dnf swap -y centos-stream-release horizon-release
 dnf install -y --setopt=install_weak_deps=False uupd krunner-bazaar ublue-brew
